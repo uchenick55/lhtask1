5a1026f408f1baea999ab7a6d6feed5f1d487872
uchenick55 <uchenick55@yandex.ru>
2023-01-09 21:28:24 +0300

    рефактор (commonPart1)

--- src/LHtask1/lhtask1.jsx
+++ src/LHtask1/lhtask1.jsx
@@ -17,53 +17,59 @@ function sostavChisla(massivChisel, chislo) { //[8, 2, 3, 4, 6, 7, 1],
 
     // код писать только внутри данной функции
     let resultLocal = []; // определение массива результата
-    let massivChisel2 = [];
-    let massivChisel3 = [];
-    let massivChisel4 = [];
-    let massivChisel5 = [];
-    let itemForRecord2 = [];
-    let itemForRecord3 = [];
-    massivChisel.map(m1 => {
-        if (m1 > chislo) {
-            return
-        } // значение массива больше или равно сумме, пропускаем
-        if (m1 === chislo) {
-            resultLocal.push([m1])
+    let massivChisel2 = []; // удаляем числа больше chislo и вносим в resultLocal числа равные ему
+    let massivChisel3 = []; // прогон массива для проверки 2 слагаемых
+    let massivChisel4 = []; // прогон массива для проверки 3 слагаемых
+    let massivChisel5 = []; //прогон массива для проверки 4 слагаемых
+    let itemForRecord = []; // элемент для записи в массив перед проверкой на повторение
+    let result = []; // проверка, был ли данный элемент ранее записан в итоговый массив resultLocal
+
+    massivChisel.map(m1 => { //
+        if (m1 > chislo) {// значение массива больше или равно сумме,
+            return //  пропускаем
+        }
+        if (m1 === chislo) {  // проверка одного элемента
+            itemForRecord = [m1]; // элемент для записи в итоговый массив
+            result = resultLocal.find((item, index) => //  проверка повторений элемента
+                JSON.stringify(item) === JSON.stringify(itemForRecord));
+            if (!result) { // если данный элемент (массив) еще не записан в resultLocal
+                resultLocal.push([m1]) // записываем число как массив в итоговый массив resultLocal
+            }
         } // значение добавляем в итоговый массив
         massivChisel2.push(m1)
     })
+
+    let commonPart1 = (uslovie, itemForRecordLocal) => {
+        // общая часть для проверки условия вхождения и элемент для записи
+        if (uslovie) { // проверка двух слагаемых
+            itemForRecord = itemForRecordLocal.sort(); // элемент для записи в итоговый массив (сортируем для проверки повторений)
+            result = resultLocal.find((item, index) => //  проверка повторений элемента
+                JSON.stringify(item) === JSON.stringify(itemForRecord));
+            if (!result) { // если данный элемент (массив) еще не записан в resultLocal
+                resultLocal.push(itemForRecord)// записываем оба числа как массив в итоговый массив resultLocal
+                console.log("itemForRecord", itemForRecord)// вывод того, что добавляем
+            }
+        }
+    }
+
     massivChisel2.sort() // сортируем временный массив
-    massivChisel3 = massivChisel2.slice() // поолностью копируем массив перед прогоном
+    massivChisel3 = massivChisel2.slice() // копия массива для проверки 2 слагаемых
     massivChisel2.map(m2 => { // прогоняем второй массив
+
         massivChisel3 = massivChisel3.filter(item => item !== m2) // удаляем записаные числа из проверяемого массива
         massivChisel3.map(m3 => { // прогоняем третий массив и сравниваем с проверяемым числом
-            if (m2 + m3 === chislo) { // проверка двух слагаемых
-                itemForRecord2 = [m2, m3].sort();
-                let result = resultLocal.find((item, index) => //  проверка повторений
-                    JSON.stringify(item) === JSON.stringify(itemForRecord2));
-                if (!result) { // если данный массив еще не записан в resultLocal
-                    resultLocal.push([m2, m3].sort()) // создаем массив из нужных чисел, сортируем для выявления повторений и добавляем в итоговый массив
-                    console.log("itemForRecord2", itemForRecord2)// вывод того, что добавляем
-                }
-            }
+
+            commonPart1(m2 + m3 === chislo, [m2, m3]) // проверка 2 слагаемых
+
             if (m2 + m3 < chislo) { //если сумма 2 чисел меньше нужного результата
                 massivChisel4 = massivChisel3.filter(item => item !== m3) // копируем массив3 перед прогоном, в нем уже нет вышестоящих чисел
                 massivChisel4.map(m4 => {
-                        if (m2 + m3 + m4 === chislo) { // проверка трех слегаемых
-                            itemForRecord3 = [m2, m3, m4].sort(); // записываем числа в массив и сортируем (для проверки повторения)
-                            let result = resultLocal.find((item, index) => //  проверка повторений
-                                JSON.stringify(item) === JSON.stringify(itemForRecord3));
-                            if (!result) { // если данный массив еще не записан в resultLocal
-                                resultLocal.push(itemForRecord3)// записываем все три числа как массив в итоговый массив resultLocal
-                                console.log("itemForRecord3", itemForRecord3)
-                            }
-                        }
-                        
+
+                        commonPart1(m2 + m3 + m4 === chislo, [m2, m3, m4]) // проверка 3 слагаемых
+
                     }
                 )
             }
-
-
         })
     })
 
